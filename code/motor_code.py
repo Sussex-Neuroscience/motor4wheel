e1edeb51c4e3a05d94357e744a519b3f383ad376
Andre Maia Chagas <andremaia.chagas@gmail.com>
2023-01-25 16:59:06 +0000

    add first micropython code

---
+++ code/motor_code.py
@@ -0,0 +1,32 @@
+from machine import Pin
+from time import sleep
+
+IN1 = Pin(15,Pin.OUT)
+IN2 = Pin(2,Pin.OUT)
+IN3 = Pin(16,Pin.OUT)
+IN4 = Pin(17,Pin.OUT)
+
+pins = [IN1,IN2,IN3,IN4]
+pot = ADC(Pin(35))
+pot.atten(ADC.ATTN_11DB)
+
+sequence = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+oldMax = 4095
+oldMin = 0
+newMax = 1.0 //change this to modify wheel top speed (lower values make it spin faster)
+newMin = 0
+oldRange = (oldMax - oldMin)  
+newRange = (newMax - newMin)  
+
+while True:
+    for step in sequence:
+        for i in range(len(pins)):
+            pins[i].value(step[i])
+            sleep(0.001)
+        potValue = pot.read()
+        print(potValue)
+        
+        
+        newValue = (((potValue - oldMin) * newRange) / oldRange) + newMin
+        sleep(newValue)
